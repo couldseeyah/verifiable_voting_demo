3b82a3f1bd740c026fbf5d3ddefb9b6ebb8168da
Qudsia tariw <tariq.qudsia18@gmail.com>
2024-11-23 20:36:54 +0500

    extract rand func

--- encryption.py
+++ encryption.py
@@ -1,4 +1,5 @@
 from lightphe.cryptosystems.Paillier import Paillier
+from sympy import mod_inverse, gcd
 
 class Ciphertext:
     def __init__(self, ciphertext: int, randomness: int):
@@ -20,14 +21,17 @@ class Ciphertext:
 
 
 class Encryption:
-    def __init__(self, public_key: int, private_key: int):
+    def __init__(self, public_key: int = None, private_key: int = None):
         """
         Initialize the Encryption class with a public and private key.
         
         :param public_key: The public key used for encryption.
         :param private_key: The private key used for decryption.
         """
-        self.paillier = Paillier({private_key: private_key, public_key: public_key})
+        if public_key is None and private_key is None:
+            self.paillier = Paillier()
+        else:
+            self.paillier = Paillier({private_key: private_key, public_key: public_key})
     
     def generate_random_key(self):
         """
@@ -59,6 +63,37 @@ class Encryption:
         combined_randomness = ct1.randomness*ct2.randomness
         ciphertext_object = Ciphertext(sum, combined_randomness)
         return ciphertext_object
+
+    def obtain_ephermeral_key(self, ct):
+        """
+        Obtain an ephemeral key.
+        """
+        # Compute g^λ mod n^2
+        g_lambda_mod_n_squared = pow((self.paillier.plaintext_modulo)+1, self.paillier.keys["private_key"]["phi"], self.paillier.ciphertext_modulo)
+        
+        # Compute L(g^λ mod n^2)
+        g_lambda_L = (g_lambda_mod_n_squared - 1) // self.paillier.plaintext_modulo
+
+        # Compute c^λ mod n^2
+        c_lambda_mod_n_squared = pow(ct.ciphertext, self.paillier.keys["private_key"]["phi"], self.paillier.ciphertext_modulo)
+
+        # Compute L(c^λ mod n^2)
+        c_lambda_L = (c_lambda_mod_n_squared - 1) // self.paillier.plaintext_modulo
+
+        # Recover plaintext m
+        m = (c_lambda_L * mod_inverse(g_lambda_L, self.paillier.plaintext_modulo)) % self.paillier.plaintext_modulo
+
+        # Step 2: Recover r^n from c and m
+        g_m_mod_n_squared = pow((self.paillier.plaintext_modulo)+1, m, self.paillier.ciphertext_modulo)  # g^m mod n^2
+        g_m_inv_mod_n_squared = mod_inverse(g_m_mod_n_squared, self.paillier.ciphertext_modulo)  # (g^m)^-1 mod n^2
+        r_n = (ct.ciphertext * g_m_inv_mod_n_squared) % self.paillier.ciphertext_modulo  # r^n mod n^2
+
+        # Step 3: Recover r (n-th root of r^n modulo n)
+        for r in range(1, self.paillier.plaintext_modulo):  # Brute force over possible values of r
+            if pow(r, self.paillier.plaintext_modulo, self.paillier.ciphertext_modulo) == r_n:
+                return r
+
+        raise ValueError("Failed to recover random factor r.")
     
     def decrypt(self, ct):
         """
@@ -78,3 +113,14 @@ class Encryption:
         :return: Serialized output.
         """
         pass
+
+
+testing = Encryption()
+
+c1 = testing.encrypt(5, 2)
+c2 = testing.encrypt(10, 3)
+
+c3 = testing.add(c1, c2)
+
+print(testing.decrypt(c3))
+print(testing.obtain_ephermeral_key(c3))
